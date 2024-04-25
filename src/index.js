83cdba00fca177ae5387c98b828cb7b86575b27e
Zandile442 <133495331+Zandile442@users.noreply.github.com>
2024-04-25 05:09:46 +0200

    added the weather api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateweather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchInput(city) {
+  let apiKey = "89fe3ee5oa7abd07t90e6b4d14bc6f7e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units-metrics`;
+  axios.get(apiUrl).then(updateweather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
